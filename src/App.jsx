178acdd2e31b20f630fd77a8bc4327eeea47ab4a
GeorgeVourkas <geovou04@gmail.com>
2025-09-27 10:34:05 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,36 @@ import BackgroundView from "./components/BackgroundView";
 import InformationSection from "./components/InformationSection";
 import PopularCities from "./components/PopularCities";
 import "./styles/style.css"
-import favoriteCities from "./favorites";
 export default function App() {
-  const [citiesWeather, setCitiesWeather] = React.useState([]);
-  const [selectedCity, setSelectedCity] = React.useState(null);
+  const [favCities,setFavCities] = React.useState([]);
 
-  React.useEffect(() => {
-    async function fetchWeather() {
-      const apiKey = "69c7daa9223cbc43fab0367cc0f76034";
-      const promises = favoriteCities.map(async (city) => {
-        const res = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-        );
-        return res.json();
-      });
-      const results = await Promise.all(promises);
-      setCitiesWeather(results);
-      if (results.length > 0) setSelectedCity(results[0]);
-    }
-    fetchWeather();
-  }, []);
+  // const [citiesWeather, setCitiesWeather] = useState([]);
+
+  // useEffect(() => {
+  //   async function fetchWeather() {
+  //       const apiKey = "69c7daa9223cbc43fab0367cc0f76034";
+
+  //       // Fetch weather for all favorite cities
+  //       const promises = favoriteCities.map(async (city) => {
+  //       const res = await fetch(
+  //           `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  //       );
+  //       return res.json();
+  //       });
+
+  //     const results = await Promise.all(promises);
+  //     setCitiesWeather(results);
+  //   }
+
+  //   fetchWeather();
+  // }, []);
   return (
   
       <section className="mainSection">
         <Header/>
         <BackgroundView/>
-        <InformationSection city={selectedCity}/>
-        <PopularCities cities={citiesWeather} onSelect={setSelectedCity}/>
+        <InformationSection/>
+        <PopularCities/>
       
       </section>
   
